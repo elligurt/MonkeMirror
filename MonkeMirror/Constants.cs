ac75c2c1782d6968a457f1b6b1467264be0bcb0f
elliot! <benjikrolfanpageuwu@gmail.com>
2025-08-06 19:03:33 +0100

    Bump Version

--- MonkeMirror/Constants.cs
+++ MonkeMirror/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.elliot.monkemiror";
         public const string Name = "MonkeMirror";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
     }
 }
