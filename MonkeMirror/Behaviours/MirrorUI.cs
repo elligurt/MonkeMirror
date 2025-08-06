eb1456d485dbefb6a076a28004784c4f15c40845
elliot! <benjikrolfanpageuwu@gmail.com>
2025-08-06 19:01:54 +0100

    Add files via upload

---
+++ MonkeMirror/Behaviours/MirrorUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+namespace MonkeMirror.Behaviours
+{
+    public class MirrorUI : MonoBehaviour
+    {
+        private TMP_Text nameText;
+
+        private void Awake()
+        {
+            var nameTextTransform = transform.Find("UI/Name Text");
+            if (nameTextTransform != null)
+                nameText = nameTextTransform.GetComponent<TMP_Text>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(SetNameWhenReady());
+        }
+
+        private IEnumerator SetNameWhenReady()
+        {
+            yield return new WaitUntil(() => PhotonNetwork.LocalPlayer != null);
+            yield return new WaitForSeconds(1f);
+
+            if (nameText != null)
+            {
+                string nickname = PhotonNetwork.LocalPlayer.NickName;
+                nameText.text = string.IsNullOrEmpty(nickname) ? "???" : nickname;
+            }
+        }
+    }
+}
