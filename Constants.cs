0bb7172d23df50bf5059d8b9948f3fe952baf92d
elliot! <benjikrolfanpageuwu@gmail.com>
2025-10-05 15:41:58 +0100

    Rewrite/Upload cause I messed up this repo

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeMirror
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class Constants
+    {
+        public const string GUID = "com.elliot.gorillatag.monkemirror";
+        public const string Name = "MonkeMirror";
+        public const string Version = "1.0.0";
+    }
+}
