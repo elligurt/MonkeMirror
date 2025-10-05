a08a7f62e5586b608b0a44e3629a496f1c4a90c9
elliot! <benjikrolfanpageuwu@gmail.com>
2025-10-05 15:39:10 +0100

    Delete MonkeMirror directory

--- MonkeMirror/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MonkeMirror
-{
-    public static class Constants
-    {
-        public const string GUID = "com.elliot.monkemiror";
-        public const string Name = "MonkeMirror";
-        public const string Version = "1.0.2";
-    }
-}
