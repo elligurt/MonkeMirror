2eba3fc2887c8c45778646a9892c231f264f0446
elliot! <benjikrolfanpageuwu@gmail.com>
2025-10-05 15:40:03 +0100

    Rewrite/Upload cause I messed up this repo

---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+using MonkeMirror.Tools;
+using MonkeMirror.Behaviours;
+
+namespace MonkeMirror
+{
+    [BepInPlugin(Constants.GUID, Constants.Name, Constants.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        private GameObject _mirrorPrefab;
+
+        private void Awake()
+        {
+            Instance = this;
+            _ = SetupMirror();
+        }
+
+        private async Task SetupMirror()
+        {
+            try
+            {
+                _mirrorPrefab = await AssetLoader.LoadAsset<GameObject>("MonkeMirror");
+                if (_mirrorPrefab == null)
+                {
+                    Debug.LogError("[MonkeMirror] Failed to load prefab.");
+                    return;
+                }
+
+                GameObject mirrorInstance = Instantiate(_mirrorPrefab);
+                mirrorInstance.SetActive(true);
+                mirrorInstance.transform.position = new Vector3(-65.5466f, 10.2129f, - 80.04f);
+                mirrorInstance.transform.rotation = Quaternion.Euler(2.5711f, 349.3142f, 3.8056f);
+                mirrorInstance.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f); //by the friends stand
+
+                mirrorInstance.AddComponent<MirrorUI>();
+
+                var button = mirrorInstance.transform.Find("UIButton/Button")?.gameObject;
+                if (button != null)
+                    button.AddComponent<PressableButton>();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("[MonkeMirror] Error setting up mirror: " + ex);
+            }
+        }
+    }
+}
---
+++ Tools/PressableButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+
+namespace MonkeMirror.Tools
+{
+    public class PressableButton : MonoBehaviour
+    {
+        public Action OnPress;
+
+        private const float DebounceTime = 0.2f;
+        private float touchTime;
+
+        private GameObject uiObject;
+        private GameObject showIndicator;
+        private GameObject hideIndicator;
+
+        private void Awake()
+        {
+            gameObject.SetLayer(UnityLayer.GorillaInteractable);
+
+            uiObject = transform.root.Find("UI")?.gameObject;
+            showIndicator = transform.root.Find("UIButton/ShowUI")?.gameObject;
+            hideIndicator = transform.root.Find("UIButton/HideUI")?.gameObject;
+
+            if (uiObject != null)
+            {
+                if (uiObject.activeSelf)
+                {
+                    if (hideIndicator != null) hideIndicator.SetActive(true);
+                    if (showIndicator != null) showIndicator.SetActive(false);
+                }
+                else
+                {
+                    if (hideIndicator != null) hideIndicator.SetActive(false);
+                    if (showIndicator != null) showIndicator.SetActive(true);
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (Time.time - touchTime < DebounceTime)
+                return;
+
+            var hand = other.GetComponent<GorillaTriggerColliderHandIndicator>();
+            if (hand == null) return;
+
+            touchTime = Time.time;
+
+            GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(67, hand.isLeftHand, 0.05f);
+            GorillaTagger.Instance.StartVibration(
+                hand.isLeftHand,
+                GorillaTagger.Instance.tapHapticStrength / 2f,
+                GorillaTagger.Instance.tapHapticDuration
+            );
+
+            OnPress?.Invoke();
+
+            if (uiObject == null)
+            {
+                Debug.LogWarning("[MonkeMirror] UI not found");
+                return;
+            }
+
+            bool newState = !uiObject.activeSelf;
+            uiObject.SetActive(newState);
+
+  
+            if (newState)
+            {
+
+                if (hideIndicator != null) hideIndicator.SetActive(true);
+                if (showIndicator != null) showIndicator.SetActive(false);
+            }
+            else
+            {
+                if (hideIndicator != null) hideIndicator.SetActive(false);
+                if (showIndicator != null) showIndicator.SetActive(true);
+            }
+        }
+    }
+}
